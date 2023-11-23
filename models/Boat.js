69eb95d443c94972f025f596cb382a3b7d8e3f0b
Mouad Anouch <md.anouchpro@gmail.com>
2023-11-23 11:59:24 +0100

    add request data check

--- models/Boat.js
+++ models/Boat.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const boatSchema = new mongoose.Schema({
-    name: String,
-    type: String,
-    length: Number,
-    yearBuilt: Number,
-    // add more fields as needed
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    length: { type: Number, required: true },
+    yearBuilt: { type: Number, required: true },
+    // add more fields as needed, marking them as required if necessary
 });
 
 const Boat = mongoose.model('Boat', boatSchema);
