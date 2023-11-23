e0b465fe2820bf3b6dd42b7b7a82b23927902932
Christophe <pro.chrisrln@gmail.com>
2023-11-23 15:04:10 +0100

    Chris: User

---
+++ controllers/userController.js
@@ -0,0 +1,65 @@
+const User = require('../models/User');
+
+const userController = {
+  // POST - Créer un nouvel utilisateur
+  createUser: async (req, res) => {
+    try {
+      const user = new User(req.body);
+      await user.save();
+      res.status(201).send(user);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  },
+
+  // GET - Récupérer tous les utilisateurs
+  getUsers: async (req, res) => {
+    try {
+      const users = await User.find({});
+      res.send(users);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  },
+
+  // GET - Récupérer un utilisateur par ID
+  getUser: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  },
+
+  // PATCH - Mettre à jour un utilisateur
+  updateUser: async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  },
+
+  // DELETE - Supprimer un utilisateur
+  deleteUser: async (req, res) => {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+}
+
+module.exports = userController;
