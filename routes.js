be28a0d52f5f6801cba29ef9dbae17df4efc2767
cormorecheleo <leocormo@gmail.com>
2023-12-07 14:39:35 +0100

    BF-005 : Creation/Modification  Bateau

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
 const boatController = require('./controllers/boatController'); // Path to your boatController
+const tripController = require('./controllers/tripController');
+
+// Router
 const router = express.Router();
 
+/** BOAT ROUTES */
 // POST route to create a boat
 router.post('/boats', boatController.createBoat);
 
@@ -13,6 +18,15 @@ router.delete('/boats/:boatId', boatController.deleteBoat);
 
 // GET route to search for boats
 router.get('/boats/search', boatController.searchBoats);
+
+
+/** TRIP ROUTES */
+
+// POST route
+router.post('/trips', tripController.createTrip);
+
+// PUT update trip
+router.put('/trips/:tripId', tripController.updateTrip);
 
 // ... include this router in your main server file
 module.exports = router;
