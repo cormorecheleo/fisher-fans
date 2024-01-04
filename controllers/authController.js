bdf2d33c07fd52ce7cccfe205b642391e9ba666c
Mouad A <56634799+MD-VA@users.noreply.github.com>
2024-01-04 13:53:01 +0100

    [BF-003] authentification + boat creation (#33)

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+// controllers/authController.js
+
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const signup = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+      throw new Error('Unable to login');
+    }
+
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+module.exports = {
+  signup,
+  login,
+};
--- controllers/boatController.js
+++ controllers/boatController.js
@@ -1,25 +1,18 @@
-const Joi = require('joi');
 const Boat = require('../models/Boat'); // Ensure the path is correct
 
-// Joi validation schemas
-const boatSchema = Joi.object({
-    name: Joi.string().required(),
-    type: Joi.string().required(),
-    length: Joi.number().required(),
-    yearBuilt: Joi.number().required(),
-    // Add more fields as needed
-});
-
-const updateBoatSchema = boatSchema.fork(Object.keys(boatSchema.describe().keys), field => field.optional());
-
 const boatController = {
+
+    getAllBoats: async (req, res) => {
+        try {
+            const boats = await Boat.find({});
+            res.json(boats);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    },
     // Create a new boat
     createBoat: async (req, res) => {
         try {
-            const validationResult = boatSchema.validate(req.body);
-            if (validationResult.error) {
-                return res.status(400).json({ message: validationResult.error.details[0].message });
-            }
             const newBoat = new Boat(req.body);
             const savedBoat = await newBoat.save();
             res.status(201).json(savedBoat);
@@ -31,10 +24,6 @@ const boatController = {
     // Update a boat
     updateBoat: async (req, res) => {
         try {
-            const validationResult = updateBoatSchema.validate(req.body);
-            if (validationResult.error) {
-                return res.status(400).json({ message: validationResult.error.details[0].message });
-            }
             const updatedBoat = await Boat.findByIdAndUpdate(req.params.boatId, req.body, { new: true });
             res.json(updatedBoat);
         } catch (err) {
@@ -63,6 +52,8 @@ const boatController = {
             res.status(400).json({ message: err.message });
         }
     }
+
+    
 };
 
 module.exports = boatController;
