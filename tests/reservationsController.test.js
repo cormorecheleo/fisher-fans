9db87704b9c95677ef799aa48238e318d8d6ed28
Christophe <pro.chrisrln@gmail.com>
2024-01-18 11:30:10 +0100

    Chris: reservations tests

--- tests/reservationsController.test.js
+++ tests/reservationsController.test.js
@@ -1,121 +1,88 @@
-// const request = require('supertest');
-// const app = require('../server');
-// const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../server'); 
+const mongoose = require('mongoose');
+const Reservation = require('../models/Reservation');
 
-// let authToken;
-// let reservationId;
-// let user;
-// let testObject = {
-//   Description: 'Luxury yacht with modern amenities',
-//   Marque: 'YachtMaker',
-//   AnneeDeFabrication: 2020,
-//   URLDeLaPhoto: 'http://example.com/photo.jpg',
-//   TypeDePermisRequis: 'Yacht',
-//   TypeDeBateau: 'Yacht',
-//   Equipements: 'GPS, Radar, Life Jackets',
-//   MontantDeLaCaution: 5000,
-//   CapaciteMaximum: 10,
-//   NombreDeCouchages: 5,
-//   PortDAttache: 'Marina Bay',
-//   PaysDAttache: 'France',
-//   Latitude: 43.2951,
-//   Longitude: 5.3631,
-//   TypeDeMotorisation: 'Inboard',
-//   PuissanceDuMoteur: 350,
-// };
-// describe('Reservation Controller Tests', () => {
-//   beforeAll(async () => {
-//     // Connect to the MongoDB database before running the tests
-//     await mongoose.connect(process.env.MONGODB_URI);
-//     await request(app).post('/signup').send({
-//       name: 'Test User',
-//       email: 'test@example.com',
-//       password: 'password123',
-//     });
+let token;
+let testReservation = {
+  DateRetenue: new Date(),
+  NombreDePlacesReservees: 5,
+  PrixTotal: 100,
+  trip: "655f6eb7b6e7c1002a4e1f4d", 
+};
 
-//     const response = await request(app).post('/login').send({
-//       email: 'test@example.com',
-//       password: 'password123',
-//     });
+describe('Reservation Controller Tests', () => {
+  let reservation;
+  
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
 
-//     authToken = response.body.token;
-//     user = response.body.user;
-//     console.log("USER", user)
-//     testObject = { ...testObject, createdBy: response.body.user._id };
-//   }, 50000);
+    const response = await request(app)
+      .post('/auth/login')
+      .send({
+        email: "jesttests@doe.com",
+        password: "yourpassword"
+      });
 
-//   afterAll(async () => {
-//     await mongoose.disconnect();
-//   });
+    token = response.body.token;
+  }, 50000);
 
-//   describe('Reservation Routes', () => {
-//     let boatId;
+  beforeEach(async () => {
+  });
 
-//     beforeAll(async () => {
-//       // Create a test boat before running the reservation tests
-//       const boatResponse = await request(app)
-//         .post('/boats')
-//         .set('Authorization', `Bearer ${authToken}`)
-//         .send({
-//           Nom: 'Test Boat',
-//           ...testObject,
-//         });
+  afterAll(async () => {
+    await Reservation.deleteOne({ DateRetenue: testReservation.DateRetenue });
+    await mongoose.connection.close();
+  });
 
-//       boatId = boatResponse.body._id;
-//     });
+  it('should create a new reservation', async () => {
+    const response = await request(app)
+      .post('/reservations')
+      .set('Authorization', `Bearer ${token}`)
+      .send({...testReservation})
+      .expect(201);
 
-//     it('should create a reservation', async () => {
-//       const response = await request(app)
-//         .post('/reservations')
-//         .set('Authorization', `Bearer ${authToken}`)
-//         .send({
-//           DateRetenue: '2024-01-15',
-//           NombreDePlacesReservees: 5,
-//           PrixTotal: 150.75,
-//           userId: user._id, // Replace with the actual user ID
-//           boatId,
-//         });
+    reservation = response.body;
+    expect(response.body.DateRetenue).toBeDefined();
+  }, 10000);
 
-//       expect(response.statusCode).toBe(201);
-//       expect(response.body).toHaveProperty('DateRetenue', '2024-01-15T00:00:00.000Z');
-//       reservationId = response.body._id; // Store the reservation ID for subsequent tests
-//     });
+  it('should get all reservations', async () => {
+    const response = await request(app)
+      .get('/reservations')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
 
-//     it('should get a reservation by ID', async () => {
-//       const response = await request(app)
-//         .get(`/reservations/${reservationId}`)
-//         .set('Authorization', `Bearer ${authToken}`);
+    expect(response.body.length).toBeGreaterThanOrEqual(1);
+  }, 10000);
 
-//       expect(response.statusCode).toBe(200);
-//       expect(response.body).toHaveProperty('_id', reservationId);
-//       // Include other expected properties in the response body
-//     });
+  it('should get a reservation by ID', async () => {
+    const response = await request(app)
+      .get(`/reservations/${reservation._id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
 
-//     it('should update a reservation', async () => {
-//       const response = await request(app)
-//         .put(`/reservations/${reservationId}`)
-//         .set('Authorization', `Bearer ${authToken}`)
-//         .send({
-//           DateRetenue: '2024-01-16',
-//           NombreDePlacesReservees: 3,
-//           PrixTotal: 100.5,
-//         });
+    expect(response.body.DateRetenue).toEqual(reservation.DateRetenue);
+  }, 10000);
 
-//       expect(response.statusCode).toBe(200);
-//       expect(response.body).toHaveProperty('DateRetenue', '2024-01-16T00:00:00.000Z');
-//       // Include other expected properties in the response body
-//     });
+  it('should update a reservation', async () => {
+    const response = await request(app)
+      .put(`/reservations/${reservation._id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        PrixTotal: 120.0,
+      })
+      .expect(200);
 
-//     it('should delete a reservation', async () => {
-//       const response = await request(app)
-//         .delete(`/reservations/${reservationId}`)
-//         .set('Authorization', `Bearer ${authToken}`);
+    expect(response.body.PrixTotal).toBe(120.0);
+  }, 10000);
 
-//       expect(response.statusCode).toBe(200);
-//       expect(response.body).toHaveProperty(
-//         'message',
-//         'Reservation deleted successfully'
-//       );
-//     });
-//   });
-// });
+  it('should delete a reservation', async () => {
+    await request(app)
+      .delete(`/reservations/${reservation._id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+
+    const deletedReservation = await Reservation.findById(reservation._id);
+    expect(deletedReservation).toBeNull();
+  }, 10000);
+});
