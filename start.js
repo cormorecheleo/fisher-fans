e3ed195f9ce2e1d79d1d42fd9d12b9d2f68ca00d
Christophe <pro.chrisrln@gmail.com>
2024-01-18 10:31:23 +0100

    Chris: https

--- start.js
+++ start.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
+const https = require('https');
+const fs = require('fs');
 const app = require('./server');
 
 const port = process.env.PORT || 3000;
 
+// Charger le certificat SSL et la clé privée
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
+    // Créer un serveur HTTPS
+    https.createServer(options, app).listen(port, () => {
+        console.log(`HTTPS Server listening on port ${port}`);
     });
 })
 .catch(err => console.log(err));
